b58cd7088bd35be9fa63f965f4a752f9e971a88b
ZhouTian0716 <egglord0716@gmail.com>
2021-08-12 19:20:23 +1000

    nav bar ready

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
+import Header from './components/nav/Header';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component={Register}/>
-    </Switch> 
+    <>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+      </Switch> 
+    </>
   )
 }
